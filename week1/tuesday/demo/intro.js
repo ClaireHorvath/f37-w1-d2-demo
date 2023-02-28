c6d1fde6489c4850250ad7fa04e7ee25d8d20620
Claire Horvath <horvathclaire16@gmail.com>
2023-02-28 17:42:32 -0500

    added demo file

---
+++ week1/tuesday/demo/intro.js
@@ -0,0 +1,36 @@
+
+
+// console.log("Hello, world");
+
+const firstName = "Claire";
+
+// console.log(firstName);
+
+let myAge = 26;
+
+const isAwesome = true;
+
+// console.log(myAge, isAwesome);
+
+// console.log("Hello, my name is " + firstName + " I am " + myAge + " years old");
+
+// console.log(`Hello, my name is ${firstName} I am ${myAge} years old.`);
+
+
+let x = 4;
+let y = 10;
+let z = x + 8;
+
+let sum = x + y + z;
+
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// console.log(sum);
+
+if (x === y) {
+    console.log('X and Y are the same value')
+} else {
+    console.log('X and Y are different values')
+}
